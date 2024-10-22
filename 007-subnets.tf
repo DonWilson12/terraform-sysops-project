63c0ce8c9e515cd14a8bd6d1eaa79fc08886af88
Admin <donwilsonobido@gmail.com>
2024-10-23 01:11:04 +0800

    update the subnets.tf file

--- 007-subnets.tf
+++ 007-subnets.tf
@@ -1,70 +1,70 @@
 # Creating public subnets
 resource "aws_subnet" "public-1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = var.ZONE1
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = var.ZONE1
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
 
 resource "aws_subnet" "public-2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = var.ZONE2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = var.ZONE2
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
 
 # Creating application subnets
 resource "aws_subnet" "app-1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
   availability_zone = var.ZONE1
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
 
 resource "aws_subnet" "app-2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
   availability_zone = var.ZONE2
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
 
 # Creating backend subnets
 resource "aws_subnet" "backend-1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.5.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.5.0/24"
   availability_zone = var.ZONE1
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
 
 resource "aws_subnet" "backend-2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.6.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.6.0/24"
   availability_zone = var.ZONE2
 
   tags = {
-    Name = var.project_name
+    Name        = var.project_name
     Environment = var.EnvType
   }
 }
